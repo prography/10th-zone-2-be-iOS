96d9de70b60c4087e454a63855ec980119b59e8a
강창혁 <dpkyle97@gmail.com>
2025-05-29 21:58:35 +0900

    Init: 프로젝트 초기 생성

---
+++ Zone2Be/LoginSelectView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginSelectView.swift
+//  Zone2Be
+//
+//  Created by kangChangHyeok on 29/05/2025.
+//
+
+import SwiftUI
+
+struct LoginSelectView: View {
+    var body: some View {
+        Text("LoginSelectView")
+    }
+}
+
+#Preview {
+    LoginSelectView()
+}
--- Zone2Be/Zone2BeApp.swift
+++ Zone2Be/Zone2BeApp.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 @main
 struct Zone2BeApp: App {
+    
+    let isLoggedIn = false
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            if isLoggedIn {
+                ContentView()
+                    .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            } else {
+                LoginSelectView()
+            }
         }
     }
 }
